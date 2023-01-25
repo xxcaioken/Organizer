7f3e4893e464f244be484fbc0b0de3508e260313
Caio Wendhausem Kormives <ckormives@tech6group.com>
2023-01-24 21:10:55 -0300

    first commit :)

--- Organizer/Controllers/HomeController.cs
+++ Organizer/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace Organizer.Controllers
         {
             return View();
         }
+        public IActionResult Organizador()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
